af0c7cbe365cf3fa3755caf69446e44f0f74f9a8
Mike Hummel <mh@mhus.de>
2015-01-14 23:30:30 +0100

    fix date parser, add german and uk date format

--- mhu-lib-core/src/main/java/de/mhus/lib/core/cast/ObjectToCalendar.java
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/cast/ObjectToCalendar.java
@@ -44,6 +44,10 @@ public class ObjectToCalendar implements Caster<Object,Calendar>{
 	public static Calendar toCalendar(String in) {
 		if (in == null) return null;
 		try {
+			
+			Calendar c = Calendar.getInstance();
+			c.clear();
+
 			String date = in.trim();
 
 			int hour = 0;
@@ -52,6 +56,7 @@ public class ObjectToCalendar implements Caster<Object,Calendar>{
 			int millies = 0;
 			String zone = null;
 
+			// check if date and time
 			char sep = '?';
 			if ( MString.isIndex(date, '_' ) )
 				sep = '_';
@@ -60,8 +65,7 @@ public class ObjectToCalendar implements Caster<Object,Calendar>{
 				sep = ' ';
 			
 			if (sep != '?') {
-
-				// is time part on the end....
+				// found also time ... parse it !
 				String time = MString.afterIndex(date, sep).trim();
 				date = MString.beforeIndex(in, sep).trim();
 
@@ -92,13 +96,16 @@ public class ObjectToCalendar implements Caster<Object,Calendar>{
 						sec = toint(parts[2], 0);
 				}
 
+				c.set(Calendar.HOUR_OF_DAY, hour);
+				c.set(Calendar.MINUTE, min);
+				c.set(Calendar.MILLISECOND, sec * 1000 + millies);
+				
 			}
 
-			
+			// parse the date
 			if ( date.indexOf('-') > 0) {
+				// technical time 2000.12.31
 				String[] parts = date.split("-");
-				Calendar c = Calendar.getInstance();
-				c.clear();
 				
 				if (zone != null) {
 					TimeZone tz = TimeZone.getTimeZone(zone);
@@ -147,16 +154,38 @@ public class ObjectToCalendar implements Caster<Object,Calendar>{
 	//				c.setTimeZone(tz);
 	//			}
 				
-				c.set(Calendar.HOUR_OF_DAY, hour);
-				c.set(Calendar.MINUTE, min);
-				c.set(Calendar.MILLISECOND, sec * 1000 + millies);
 				return c;
+			} else
+			if ( date.indexOf('.') > 0) {
+				// german time 31.12.2000
+				String[] parts = date.split("\\.");
+				if (parts.length == 3) {
+					int year = Integer.parseInt(parts[2]);
+					if (parts[2].length()==2) year = year + 2000; // will this lib life for 100 years ???
+					int month = Integer.parseInt(parts[1])-1;
+					int day   = Integer.parseInt(parts[0]);
+					c.set(year,month, day);
+					return c;
+				} else
+					return null;
+			} else
+			if ( date.indexOf('/') > 0) {
+				// france or UK 31/12/2000
+				String[] parts = date.split("/");
+				if (parts.length == 3) {
+					int year = Integer.parseInt(parts[2]);
+					if (parts[2].length()==2) year = year + 2000; // will this lib life for 100 years ???
+					int month = Integer.parseInt(parts[1])-1;
+					int day   = Integer.parseInt(parts[0]);
+					c.set(year,month, day);
+					return c;
+				} else
+					return null;
 			}
 			
 			try {
+				// timestamp
 				long ts = Long.valueOf(date);
-				Calendar c = Calendar.getInstance();
-				c.clear();
 				c.setTimeInMillis(ts);
 				return c;
 			} catch (NumberFormatException e) {}
