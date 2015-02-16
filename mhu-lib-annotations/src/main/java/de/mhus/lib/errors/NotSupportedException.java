93878843ac99a09e9e0fb37d8b154132df5b77a8
Mike Hummel <mh@mhus.de>
2015-02-16 10:58:33 +0100

    add new exception

---
+++ mhu-lib-annotations/src/main/java/de/mhus/lib/errors/NotSupportedException.java
@@ -0,0 +1,15 @@
+package de.mhus.lib.errors;
+
+public class NotSupportedException extends MRuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1520109962430808111L;
+
+	public NotSupportedException(Object... in) {
+		super(in);
+	}
+
+	
+}
