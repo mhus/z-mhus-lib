32a2ae50be10e873629943d17d4f7547c00e9c21
Mike Hummel <mh@mhus.de>
2015-05-19 17:18:44 +0200

    extension

---
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/strategy/SuccessfulMap.java
@@ -0,0 +1,41 @@
+package de.mhus.lib.core.strategy;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class SuccessfulMap extends Successful {
+
+	public SuccessfulMap(Operation operation, String msg) {
+		super(operation, msg);
+		setResult(new HashMap<>());
+	}
+
+	public SuccessfulMap(String path, String msg, long rc) {
+		super(path, msg, rc, new HashMap<>());
+	}
+
+	public SuccessfulMap(String path, String msg, long rc, String... keyValues) {
+		super(path, msg, rc, keyValues);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void put(String key, Object value) {
+		((HashMap<String,Object>)getResult()).put(key, value);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object get(String key) {
+		return ((HashMap<String,Object>)getResult()).get(key);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void remove(String key) {
+		((HashMap<String,Object>)getResult()).remove(key);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Set<String> keySet() {
+		return ((HashMap<String,String>)getResult()).keySet();
+	}
+	
+}
