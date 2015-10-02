73b1d780be96be078a61c54fe36752ee6b5636f0
Mike Hummel <mh@mhus.de>
2015-10-02 08:43:01 +0200

    db stability hack

--- mhu-lib-persistence/src/main/java/de/mhus/lib/sql/DataSourceProvider.java
+++ mhu-lib-persistence/src/main/java/de/mhus/lib/sql/DataSourceProvider.java
@@ -23,7 +23,18 @@ public class DataSourceProvider extends DbProvider {
 	@Override
 	public InternalDbConnection createConnection() throws Exception {
 		if (dataSource == null) return null;
-		Connection con = dataSource.getConnection();
+		Connection con = null;
+		try {
+			con = dataSource.getConnection();
+		} catch (Exception e) {
+			// special behavior for e.g. mysql, retry to get a connection after gc()
+			// Caused by: com.mysql.jdbc.exceptions.jdbc4.MySQLNonTransientConnectionException: Too many connections
+			if (e.getMessage().indexOf("Too many connections") > -1) {
+				System.gc();
+				con = dataSource.getConnection();
+			} else
+				throw e;
+		}
 		getDialect().prepareConnection(con);
 		return new JdbcConnection(this, con);
 	}
