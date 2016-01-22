50641912b6ae918ec4d4878475337d03d3d746d9
Mike Hummel <mh@mhus.de>
2016-01-22 13:52:35 +0100

    fix phone validation

--- mhu-lib-core/src/main/java/de/mhus/lib/core/MValidator.java
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/MValidator.java
@@ -114,15 +114,9 @@ public class MValidator {
 	public static boolean isPhoneNumber(String phone) {
 		if (MString.isEmpty(phone)) return false;
 		//validate phone numbers of format "1234567890"
-        if (phone.matches("\\d{10}")) return true;
-        //validating phone number with -, . or spaces
-        else if(phone.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}")) return true;
-        //validating phone number with extension length from 3 to 5
-        else if(phone.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")) return true;
-        //validating phone number where area code is in braces ()
-        else if(phone.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) return true;
-        //return false if nothing matches the input
-        else return false;
+        if (phone.matches("[+0-9]+\\d")) return true;
+        
+        return false;
     }
 	
 }
