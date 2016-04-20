4672238c34fe2d92b42937f3b2a390798e3498c3
Mike Hummel <mh@mhus.de>
2016-04-20 10:25:31 +0200

    change the implementation of link, using 'label' in html mode now

--- mhu-lib-vaadin/src/main/java/de/mhus/lib/vaadin/form/UiLink.java
+++ mhu-lib-vaadin/src/main/java/de/mhus/lib/vaadin/form/UiLink.java
@@ -2,11 +2,14 @@ package de.mhus.lib.vaadin.form;
 
 import com.vaadin.server.ExternalResource;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Link;
 import com.vaadin.ui.TextField;
 
 import de.mhus.lib.core.MCast;
+import de.mhus.lib.core.MXml;
 import de.mhus.lib.core.config.IConfig;
+import de.mhus.lib.core.util.Rfc1738;
 import de.mhus.lib.errors.MException;
 import de.mhus.lib.form.ComponentAdapter;
 import de.mhus.lib.form.ComponentDefinition;
@@ -15,22 +18,38 @@ import de.mhus.lib.form.UiComponent;
 
 public class UiLink extends UiVaadin {
 
+	private Object value;
+
+
+
 	@Override
 	protected void setValue(Object value) throws MException {
 		DataSource ds = getForm().getDataSource();
-		((Link)getComponentEditor()).setTargetName("_blank");
-		((Link)getComponentEditor()).setResource(new ExternalResource( MCast.toString(value) ));
-		((Link)getComponentEditor()).setCaption( ds.getString(this, "label", getConfig().getString("label", "label=Link") ) );
+//		((Link)getComponentEditor()).setTargetName("_blank");
+//		((Link)getComponentEditor()).setResource(new ExternalResource( MCast.toString(value) ));
+//		((Link)getComponentEditor()).setCaption( ds.getString(this, "label", getConfig().getString("label", "label=Link") ) );
+		
+		String label = MXml.encode( ds.getString(this, "label", getConfig().getString("label", "label=Link") ) );
+		((Label)getComponentEditor()).setCaptionAsHtml(true);
+		if (value == null)
+			((Label)getComponentEditor()).setCaption(label );
+		else {
+			String link = Rfc1738.encode( String.valueOf( value )  );
+			((Label)getComponentEditor()).setCaption("<a href=\"" + link + "\" target=_blank>" + label + "</a>" );
+		}
+		this.value = value;
 	}
 
 	@Override
 	public Component createEditor() {
-		return new Link();
+		return new Label();
+//		return new Link();
 	}
 
 	@Override
 	protected Object getValue() throws MException {
-		return ((Link)getComponentEditor()).getResource().toString();
+		return value;
+//		return ((Link)getComponentEditor()).getResource().toString();
 	}
 
 	
