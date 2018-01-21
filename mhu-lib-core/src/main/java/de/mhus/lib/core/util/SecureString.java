1967c86c94ba281ba3aac477c8bbc790b0fca62d
Mike Hummel <>
2018-01-21 22:05:28 +0100

    improve SecureString

--- mhu-lib-core/src/main/java/de/mhus/lib/core/util/SecureString.java
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/util/SecureString.java
@@ -6,13 +6,25 @@ import de.mhus.lib.core.crypt.MCrypt;
 public final class SecureString {
 
 	private byte[] data;
+	private int length;
 	
 	public SecureString(String data) {
+		this.length = data == null ? 0 : data.length();
+		if (data == null) return;
 		this.data = MCrypt.obfuscate(MString.toBytes(data));
 	}
 	
 	public String value() {
+		if (data == null) return null;
 		return MString.toString(MCrypt.unobfuscate(data));
+	}
+	
+	public int length() {
+		return length;
+	}
+
+	public boolean isNull() {
+		return data == null;
 	}
 	
 }
