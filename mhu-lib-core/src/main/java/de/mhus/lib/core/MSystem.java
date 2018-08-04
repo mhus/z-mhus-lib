6ba0cbce0121f7555141491ddc9950c400bd1cb6
Mike Hummel <>
2018-08-04 10:42:52 +0200

    provide uptime information

--- mhu-lib-core/src/main/java/de/mhus/lib/core/MSystem.java
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/MSystem.java
@@ -33,6 +33,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -41,6 +42,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.mhus.lib.core.logging.Log;
 import de.mhus.lib.core.system.IApi;
@@ -863,4 +866,56 @@ public class MSystem {
 	    return resolution.resolve();
 	}
 
+	public static long getJvmUptime() {
+		RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+		long uptime = rb.getUptime();
+		return uptime;
+	}
+
+	public static long getJvmStartTime() {
+		RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+		long uptime = rb.getStartTime();
+		return uptime;
+	}
+
+	public static long getSystemUptime() {
+		try {
+		    long uptime = -1;
+		    String os = System.getProperty("os.name").toLowerCase();
+		    if (os.contains("win")) {
+		        Process uptimeProc = Runtime.getRuntime().exec("net stats srv");
+		        BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
+		        String line;
+		        while ((line = in.readLine()) != null) {
+		            if (line.startsWith("Statistics since")) {
+		                SimpleDateFormat format = new SimpleDateFormat("'Statistics since' MM/dd/yyyy hh:mm:ss a");
+		                Date boottime = format.parse(line);
+		                uptime = System.currentTimeMillis() - boottime.getTime();
+		                break;
+		            }
+		        }
+		    } else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+		        Process uptimeProc = Runtime.getRuntime().exec("uptime");
+		        BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
+		        String line = in.readLine();
+		        if (line != null) {
+		            Pattern parse = Pattern.compile("((\\d+) days,)? (\\d+):(\\d+)");
+		            Matcher matcher = parse.matcher(line);
+		            if (matcher.find()) {
+		                String _days = matcher.group(2);
+		                String _hours = matcher.group(3);
+		                String _minutes = matcher.group(4);
+		                int days = _days != null ? Integer.parseInt(_days) : 0;
+		                int hours = _hours != null ? Integer.parseInt(_hours) : 0;
+		                int minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+		                uptime = (minutes * 60000) + (hours * 60000 * 60) + (days * 6000 * 60 * 24);
+		            }
+		        }
+		    }
+		    return uptime;
+		} catch (Exception e) {
+			log.d(e);
+			return -1;
+		}
+	}
 }
