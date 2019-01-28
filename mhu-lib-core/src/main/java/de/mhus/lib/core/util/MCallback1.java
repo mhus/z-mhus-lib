67496e982b1b6d0ea9b81d4167072141ab1a0298
Mike Hummel <>
2019-01-28 08:54:38 +0100

    add callback tools

---
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/util/MCallback1.java
@@ -0,0 +1,6 @@
+package de.mhus.lib.core.util;
+
+public interface MCallback1<T> {
+
+    void event(T object);
+}
---
+++ mhu-lib-core/src/main/java/de/mhus/lib/core/util/MCallback3.java
@@ -0,0 +1,6 @@
+package de.mhus.lib.core.util;
+
+public interface MCallback3<T,U, V> {
+
+    void event(T object1, U object2, V object3);
+}
