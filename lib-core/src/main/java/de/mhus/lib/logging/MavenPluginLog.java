5469a6df6236427bbb92fd029e15ebe295ae9fd1
Mike Hummel <mike@mhus.de>
2020-05-17 13:31:21 +0200

    fix spelling

--- lib-core/src/main/java/de/mhus/lib/logging/MavenPluginLog.java
+++ lib-core/src/main/java/de/mhus/lib/logging/MavenPluginLog.java
@@ -4,9 +4,9 @@ import org.apache.maven.plugin.AbstractMojo;
 
 import de.mhus.lib.core.logging.Log;
 
-public class MagenPluginLog extends Log {
+public class MavenPluginLog extends Log {
 
-	public MagenPluginLog(AbstractMojo owner) {
+	public MavenPluginLog(AbstractMojo owner) {
 		super(owner);
 		engine = new MavenPluginLogEngine(owner);
 	}
