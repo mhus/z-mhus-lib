304c8e7e22fc8e35b4e2faacf3f50071e67c8afe
Mike Hummel <mike.hummel@billingsolutions.de>
2020-07-30 12:11:43 +0200

    Update AnsiLogFilter.java

--- lib-junit/src/main/java/de/mhus/lib/tests/docker/AnsiLogFilter.java
+++ lib-junit/src/main/java/de/mhus/lib/tests/docker/AnsiLogFilter.java
@@ -11,13 +11,16 @@ public class AnsiLogFilter implements LogFilter {
             if ((i < 1 || array.get(i-1) != '\\') && c1 == 27) {
                 array.remove(i); // ESC
                 Byte c2 = array.remove(i); // first letter
-                if (c2 == '[' || c2 == '?') {
+                if (c2 == '[') {
                     while (array.size() > i) {
                         Byte c3 = array.remove(i); 
-                        if (c3 >= 'a' && c3 <= 'z' || c3 >= 'A' && c3 <= 'Z')
+                        if (c3 >= 'a' && c3 <= 'z' || c3 >= 'A' && c3 <= 'Z') {
+                            if (i > 0) i--;
                             break;
+                        }
                     }
-                }
+                } else
+                    if (i > 0) i--;
             }
         }
     }
