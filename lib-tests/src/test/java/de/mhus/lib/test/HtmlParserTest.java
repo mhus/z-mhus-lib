f03bd4f02e7c6e8e481f8e207809c1dead96049f
Mike Hummel <mike.hummel@billingsolutions.de>
2020-08-17 17:56:09 +0200

    update license info

--- lib-tests/src/test/java/de/mhus/lib/test/HtmlParserTest.java
+++ lib-tests/src/test/java/de/mhus/lib/test/HtmlParserTest.java
@@ -1,14 +1,16 @@
 /**
- * Copyright 2018 Mike Hummel
+ * Copyright (C) 2020 Mike Hummel (mh@mhus.de)
  *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package de.mhus.lib.test;
@@ -45,7 +47,7 @@ public class HtmlParserTest extends TestCase {
         parser.parse(in, listener);
 
         assertTrue(listener.pi.getFirst().equals("xml version=\"1.0\" encoding=\"ISO-8859-1\""));
-        assertTrue(listener.note.getFirst().equals("Edited"));
+        assertTrue(listener.note.get(1).equals("Edited")); // first node is "Copyright Note", second is "Edited"
         assertTrue(listener.open.size() == listener.close.size());
         assertTrue(listener.single.size() == 1);
         assertTrue(listener.text.getFirst().equals("You"));
